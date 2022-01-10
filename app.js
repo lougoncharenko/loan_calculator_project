9a7a773fc98804e34ff88d8215ddcf7d0550614f
lougoncharenko <93098869+lougoncharenko@users.noreply.github.com>
2022-01-10 12:26:13 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -17,7 +17,21 @@ const totalPayment=document.getElementById('total-payment');
 const totalInterest=document.getElementById('total-interest');
 
 //formulas
+const principal=parseFloat(amount.value);
+const calculatedInterest= parseFloat(interest.value)/100/12;
+const calculatedPayments= parseFloat(years.value)*12
 
+//compute monthly payment
+const x=Math.pow(1+calculatedInterest, calculatedPayments);
+const monthly=(principal*x*calculatedInterest)/ (x-1);
+if(isFinite(monthly)){
+    monthlyPayment.value=monthly.toFixed(2);
+    totalPayment.value=(monthly*calculatedPayments).toFixed(2);
+    totalInterest.value= ((monthly*calculatedPayments)-principal).toFixed(2);
+
+}else {
+    alert("please check your numbers")
+}
 
 }
 
